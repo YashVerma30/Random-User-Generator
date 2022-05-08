8058338487bec67bc4412a933e934b0e2c59720f
Yash Verma <99203211+usertestingsem6@users.noreply.github.com>
2022-05-08 16:14:16 +0530

    invoking the function
    We invoke the function fetchinformation to gather data from the array for each individual.

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
 
 
-    var url= "https://randomuser.me/api/";
+    var url= "https://randomuser.me/api/?results=10&gender=male&nat=AU";
+    var p= "";
 
-    fetch(url)
-    .then((response) => (response.json()))
-    .then(function(data){
+    fetchinformation(url); 
 
-        console.log(data);
+     function fetchinformation(url){
+        fetch(url)
+        .then((response) => (response.json()))
+        .then(function(data){
+    
+            data.results.forEach(person => {
 
-    });
+
+
+                });
+        });
+     }
 });
